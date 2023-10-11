7f9088f6ab46347671f480190363fee3e45c2205
kennmark <skyhex27@gmail.com>
2023-10-11 07:06:42 -0400

    Fix eden map shrine url error

--- src/exports/edenMSImgUrl.jsx
+++ src/exports/edenMSImgUrl.jsx
@@ -1,7 +1,7 @@
-import ms1 from '../assets/images/map-shrines/Eden/EE-MS1.jpg'
-import ms2 from '../assets/images/map-shrines/Eden/EE-MS2.jpg'
-import ms3 from '../assets/images/map-shrines/Eden/EE-MS3.jpg'
-import ms4 from '../assets/images/map-shrines/Eden/EE-MS4.jpg'
+import ms1 from '../assets/images/map-shrines/Eden/EE_MS1.jpg'
+import ms2 from '../assets/images/map-shrines/Eden/EE_MS2.jpg'
+import ms3 from '../assets/images/map-shrines/Eden/EE_MS3.jpg'
+import ms4 from '../assets/images/map-shrines/Eden/EE_MS4.jpg'
 
 export const EE_MS1 = ms1
 export const EE_MS2 = ms2
