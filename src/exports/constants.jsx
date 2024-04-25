ee248f47c5605c007c311ee59f8d4ed83007143b
kennmark <skyhex27@gmail.com>
2024-04-25 03:15:53 -0400

    Updated WL max count

--- src/exports/constants.jsx
+++ src/exports/constants.jsx
@@ -71,7 +71,7 @@ export const WL_COUNT =
   ORBIT_WL
 
 export const WB_REGULAR_SPIRITS = 49
-export const WB_TRAVELING_SPIRITS = 63
+export const WB_TRAVELING_SPIRITS = 64
 
 export const MS_HOME = 1
 
@@ -89,5 +89,5 @@ export const TOTAL_MS_COUNT = // updated January 15, 2024
   S19_NUM_MAP_SHRINES +
   MS_HOME
 
-export const WL_COUNT_DATE_UPDATED = 'March 11, 2024'
+export const WL_COUNT_DATE_UPDATED = 'April 25, 2024'
 export const MS_COUNT_DATE_UPDATED = 'January 15, 2024'
