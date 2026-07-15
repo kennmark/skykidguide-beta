9de42b7db2649043d97a5a89ef7799870eabf817
kennmark <skyhex27@gmail.com>
2026-07-15 11:29:32 +0800

    Adding routing layer for specified tab links.

--- src/pages/components/MapTabHeaderContainer.jsx
+++ src/pages/components/MapTabHeaderContainer.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import { Tab } from '@material-tailwind/react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const MapTabHeaderContainer = ({ label, value, activeTab, setActiveTab }) => {
+  const { pathname } = useLocation();
+
   return (
     
       <Tab
         key={value}
         value={value}
         onClick={() => setActiveTab(value)}
-        className={`py-3 theme-navbar
-                  ${activeTab === value ? 'text-[#233d4d]' : 'text-[#fe7f2d]'}
+        className={`py-3 theme-navbar transition-colors duration-200
+                  ${activeTab === value ? 'text-[#233d4d] bg-[#fe7f2d] rounded-lg' : 'text-[#fe7f2d]'}
         `}
       >
-        <a href={`#${value}`}>
+        {/* 2. Optimized SPA Hash Link handling */}
+        <Link 
+          to={`${pathname}#${value}`} 
+          className="w-full h-full block text-center capitalize"
+        >
           {label}
-        </a>
+        </Link>
       </Tab> 
   )
 }
